e9205f489211102d9d1f997479b02538d4570842
Naresh Pitla <nareshnanimudiraj@gmail.com>
2022-06-28 12:20:38 +0530

    title name changed

--- src/examples/Sidenav/index.js
+++ src/examples/Sidenav/index.js
@@ -182,14 +182,14 @@ function Sidenav({ color, brand, brandName, routes, ...rest }) {
       <MDBox p={2} mt="auto">
         <MDButton
           component="a"
-          href="https://www.creative-tim.com/product/material-dashboard-pro-react"
+          href="https://www.nin.res.in/index.html"
           target="_blank"
           rel="noreferrer"
           variant="gradient"
           color={sidenavColor}
           fullWidth
         >
-          upgrade to pro
+          Visit ICMR-NIN
         </MDButton>
       </MDBox>
     </SidenavRoot>
